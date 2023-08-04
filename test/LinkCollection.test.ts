663fe39a4e1eb130752157e9e4156c61bae297db
Michael Kim <michael.kim.200518@gmail.com>
2023-08-04 18:17:53 +0900

    Implement functions that add items

--- test/LinkCollection.test.ts
+++ test/LinkCollection.test.ts
@@ -1,17 +1,22 @@
 import { LinkCollection } from "../typechain-types";
 
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+
 import assert from "assert";
-import chai from "chai";
+import chai, { expect } from "chai";
 import { solidity } from "ethereum-waffle";
 
 import * as hre from "hardhat";
+import { sign } from "./Helper";
+
+import { ContractUtils } from "./ContractUtils";
 
 chai.use(solidity);
 
 describe("Test for LinkCollection", () => {
-
     const provider = hre.waffle.provider;
-    const [admin, owner] = provider.getWallets();
+    const [admin, owner, sender1, sender2] = provider.getWallets();
 
     let contract: LinkCollection;
 
@@ -22,10 +27,31 @@ describe("Test for LinkCollection", () => {
         await contract.deployTransaction.wait();
     });
 
-    it("Check nonce", async () => {
-        assert.deepStrictEqual((await contract.nonce(admin.address)).toString(), "0");
-        await contract.connect(admin).add();
-        assert.deepStrictEqual((await contract.nonce(admin.address)).toString(), "1");
+    it("Add an item", async () => {
+        const nonce = await contract.nonce(sender1.address);
+        assert.deepStrictEqual(nonce.toString(), "0");
+        const email = "abc@example.com";
+        const hash = ContractUtils.sha256String(email);
+        const signature = await sign(sender1, hash, nonce);
+        await expect(contract.connect(sender1).add(hash, sender1.address, signature))
+            .to.emit(contract, "Added")
+            .withArgs(hash, sender1.address);
+        assert.deepStrictEqual((await contract.nonce(sender1.address)).toString(), "1");
     });
 
+    it("Add an item with the same email", async () => {
+        const nonce = await contract.nonce(sender2.address);
+        const email = "abc@example.com";
+        const hash = ContractUtils.sha256String(email);
+        const signature = await sign(sender2, hash, nonce);
+        await expect(contract.connect(sender2).add(hash, sender2.address, signature)).to.be.revertedWith("E001");
+    });
+
+    it("Add an item with the same address", async () => {
+        const nonce = await contract.nonce(sender1.address);
+        const email = "def@example.com";
+        const hash = ContractUtils.sha256String(email);
+        const signature = await sign(sender1, hash, nonce);
+        await expect(contract.connect(sender1).add(hash, sender1.address, signature)).to.be.revertedWith("E002");
+    });
 });
