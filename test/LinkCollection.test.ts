6ad63f8c0f4a3bd551ae997489ee7a09f88eb161
Michael Kim <michael.kim.200518@gmail.com>
2023-08-05 21:21:58 +0900

    Fix test code

--- test/LinkCollection.test.ts
+++ test/LinkCollection.test.ts
@@ -8,7 +8,6 @@ import chai, { expect } from "chai";
 import { solidity } from "ethereum-waffle";
 
 import * as hre from "hardhat";
-import { sign } from "./Helper";
 
 import { ContractUtils } from "./ContractUtils";
 
@@ -33,20 +32,20 @@ describe("Test for LinkCollection", () => {
         assert.deepStrictEqual(nonce.toString(), "0");
         const email = "abc@example.com";
         const hash = ContractUtils.sha256String(email);
-        const signature = await sign(user1, hash, nonce);
+        const signature = await ContractUtils.sign(user1, hash, nonce);
         await expect(contract.connect(validators[0]).add(hash, user1.address, signature))
             .to.emit(contract, "Added")
             .withArgs(hash, user1.address);
         assert.deepStrictEqual((await contract.nonce(user1.address)).toString(), "1");
-        await expect(contract.toAddress(hash), user1.address);
-        await expect(contract.toHash(user1.address), hash);
+        assert.deepStrictEqual(await contract.toAddress(hash), user1.address);
+        assert.deepStrictEqual(await contract.toHash(user1.address), hash);
     });
 
     it("Add an item with the same email", async () => {
         const nonce = await contract.nonce(user2.address);
         const email = "abc@example.com";
         const hash = ContractUtils.sha256String(email);
-        const signature = await sign(user2, hash, nonce);
+        const signature = await ContractUtils.sign(user2, hash, nonce);
         await expect(contract.connect(validators[1]).add(hash, user2.address, signature)).to.be.revertedWith("E001");
     });
 
@@ -54,7 +53,7 @@ describe("Test for LinkCollection", () => {
         const nonce = await contract.nonce(user1.address);
         const email = "def@example.com";
         const hash = ContractUtils.sha256String(email);
-        const signature = await sign(user1, hash, nonce);
+        const signature = await ContractUtils.sign(user1, hash, nonce);
         await expect(contract.connect(validators[2]).add(hash, user1.address, signature)).to.be.revertedWith("E002");
     });
 
@@ -62,15 +61,15 @@ describe("Test for LinkCollection", () => {
         const email = "abc@example.com";
         const hash = ContractUtils.sha256String(email);
         const nonce1 = await contract.nonce(user1.address);
-        const signature1 = await sign(user1, hash, nonce1);
+        const signature1 = await ContractUtils.sign(user1, hash, nonce1);
 
         const nonce2 = await contract.nonce(user2.address);
-        const signature2 = await sign(user2, hash, nonce2);
+        const signature2 = await ContractUtils.sign(user2, hash, nonce2);
 
         await expect(contract.connect(validators[2]).update(hash, user1.address, signature1, user2.address, signature2))
             .to.emit(contract, "Updated")
             .withArgs(hash, user1.address, user2.address);
-        await expect(contract.toAddress(hash), user2.address);
-        await expect(contract.toHash(user2.address), hash);
+        assert.deepStrictEqual(await contract.toAddress(hash), user2.address);
+        assert.deepStrictEqual(await contract.toHash(user2.address), hash);
     });
 });
