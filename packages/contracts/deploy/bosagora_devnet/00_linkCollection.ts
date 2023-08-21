30e9852af1c72faa2fa16ec6fcedcd3eaa1ece39
Michael Kim <michael.kim.200518@gmail.com>
2023-08-21 15:10:01 +0900

    Rename package to del-osx
    Add various networks to `active_contracts.json`

---
+++ packages/contracts/deploy/bosagora_devnet/00_linkCollection.ts
@@ -0,0 +1,26 @@
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-deploy";
+// tslint:disable-next-line:no-submodule-imports
+import { DeployFunction } from "hardhat-deploy/types";
+// tslint:disable-next-line:no-submodule-imports
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { LinkCollection } from "../../typechain-types";
+
+// tslint:disable-next-line:only-arrow-functions
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    console.log(`\nDeploying LinkCollection.`);
+
+    const { deployments, getNamedAccounts, ethers } = hre;
+    const { deploy } = deployments;
+    const { deployer, owner, validator1, validator2, validator3} = await getNamedAccounts();
+    const validators = [validator1, validator2, validator3];
+
+    const deployResult = await deploy("LinkCollection", {
+        from: deployer,
+        args: [validators],
+        log: true,
+    });
+};
+
+export default func;
+func.tags = ["LinkCollection"];
