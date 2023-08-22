5fbdff71820efca0cbbae0090f0acc264f33f1f4
Michael Kim <michael.kim.200518@gmail.com>
2023-08-22 17:10:24 +0900

    Adds the ability to index registration requests and link data

--- packages/subgraph/src/utils.ts
+++ packages/subgraph/src/utils.ts
@@ -1,4 +1,6 @@
 import { BigInt, Bytes } from "@graphprotocol/graph-ts";
 
 export const AmountUnit = BigInt.fromI32(1_000_000_000);
-export const NullAccount: Bytes = Bytes.fromHexString("0x0000000000000000000000000000000000000000");
+export const NullAccount: Bytes = Bytes.fromHexString(
+  "0x0000000000000000000000000000000000000000"
+);
