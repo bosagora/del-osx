42e2acd4e7ef5528d4f72bd97a126494ac01c985
Michael Kim <michael.kim.200518@gmail.com>
2023-09-01 09:09:41 +0900

    Create a Dockerfile for validator

--- packages/validator/hardhat.config.ts
+++ packages/validator/hardhat.config.ts
@@ -131,6 +131,12 @@ const config = {
             accounts: getAccounts(),
             deploy: ["./deploy/bosagora_devnet"],
         },
+        production_net: {
+            url: process.env.PRODUCTION_NET_URL || "",
+            chainId: Number(process.env.PRODUCTION_CHAIN_ID || "2151"),
+            accounts: getAccounts(),
+            deploy: [],
+        },
     },
     namedAccounts: {
         deployer: {
